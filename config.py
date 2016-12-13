38442a97601b7f4d513096c800c137b235b0ca9d
keepwn <keepwn@gmail.com>
2016-12-13 17:09:22 +0800

    Move app.conf from data/ dir to / dir

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import configparser
 
 def get_config(section, key):
     config = configparser.ConfigParser()
-    path = 'data/app.conf'
+    path = 'app.conf'
     config.read(path)
     return os.environ.get(key.upper()) or config.get(section, key)
